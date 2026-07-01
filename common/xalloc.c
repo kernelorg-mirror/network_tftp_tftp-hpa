e30f5cd41ca1d388c3496e6c0866791e4fd09d98
H. Peter Anvin (Intel) <hpa@zytor.com>
2026-07-01 14:02:28 -0700

    common: merge xmalloc() and xstrdup() into a common function
    Merging these into a common function makes it easier to unify the
    error handling.
    
    Use perror(_progname) rather than fprintf() for the error message.
    
    Signed-off-by: H. Peter Anvin (Intel) <hpa@zytor.com>

---
+++ common/xalloc.c
@@ -0,0 +1,33 @@
+/*
+ * xmalloc.c
+ *
+ * Simple error-checking version of malloc()
+ *
+ */
+
+#include "config.h"
+#include "tftpsubs.h"
+
+static void out_of_memory(void)
+{
+    perror(_progname);
+    exit(EX_OSERR);
+}
+
+static void *check_null(void *p)
+{
+    if (!p)
+        out_of_memory();
+
+    return p;
+}
+
+void *xmalloc(size_t size)
+{
+    return check_null(malloc(size));
+}
+
+char *xstrdup(const char *s)
+{
+    return check_null(strdup(s));
+}
